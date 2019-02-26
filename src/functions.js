b624b98cb9f13ca21c7b4a681bb1b0c4abb4f54f
Alan Liu <alanliu@exosite.com>
2019-02-26 13:57:24 +0800

    implement /insight/:function_id

--- src/functions.js
+++ src/functions.js
@@ -3,121 +3,127 @@ const math = require('mathjs')
 const rp = require('request-promise')
 
 class InsightFunctions {
-    // constructor() {}
+    constructor() {
+        this._insightsInfos = {
+            "mathFormulaOne": {
+                id: "mathFormulaOne",
+                name: "One Variable Formula",
+                description: "Use Math.js to apply equation calculation. http://mathjs.org/docs/expressions/syntax.html",
+                constants: [
+                    {
+                        name: "formula",
+                        type: "string",
+                        description: "Ex: log(sqrt(x*10))"
+                    }
+                ],
+                inlets: [
+                    {
+                        data_type: "NUMBER",
+                        data_unit: "",
+                        name: "x",
+                        description: "x variable"
+                    }
+                ],
+                outlets: {
+                    data_type: "NUMBER",
+                    data_unit: "",
+                }
+            }, 
+            "mathFormulaTwo": {
+                id: "mathFormulaTwo",
+                name: "Two Variables Formula",
+                description: "Use Math.js to apply equation calculation. http://mathjs.org/docs/expressions/syntax.html",
+                constants: [
+                    {
+                        name: "formula",
+                        type: "string",
+                        description: "Ex: log(sqrt(x*10)) + y"
+                    }
+                ],
+                inlets: [
+                    {
+                        data_type: "NUMBER",
+                        data_unit: "",
+                        name: "x",
+                        description: "x variable"
+                    }, {
+                        data_type: "NUMBER",
+                        data_unit: "",
+                        name: "y",
+                        description: "y variable"
+                    }
+                ],
+                outlets: {
+                    data_type: "NUMBER",
+                    data_unit: "",
+                },
+                history: {
+                    limit: { value: 1 },
+                //     relative_start: {value: '-1d'},
+                }
+            }, 
+            "httpPost": {
+                id: "httpPost",
+                name: "HTTP Post",
+                description: "bypassing to external service",
+                constants: [
+                    {
+                        name: "url",
+                        type: "string",
+                        description: "Endpoint url"
+                    }, {
+                        name: "add_payload",
+                        type: "string",
+                        description: "Your additional payload"
+                    }
+                ],
+                inlets: [
+                    {
+                        data_type: "NUMBER",
+                        data_unit: "",
+                        name: "x",
+                        description: "x variable"
+                    }
+                ],
+                outlets: {
+                    data_type: "string",
+                    data_unit: "",
+                },
+            }, 
+            "addNumbers": {
+                id: "addNumbers",
+                name: "Add Numbers",
+                description: "Sum one data point and a user-defined value",
+                constants: [
+                    {
+                        name: "adder",
+                        type: "number"
+                    }
+                ],
+                inlets: [
+                    {
+                        data_type: "NUMBER",
+                        data_unit: "",
+                        description: "One number"
+                    }
+                ],
+                outlets: {
+                    data_type: "NUMBER",
+                    data_unit: "",
+                }
+            } 
+        }
+    }
     
     getFunctions(groupId) {
-        if(typeof groupId === 'string' && groupId.toLowerCase() === 'sigfoxhackday') {
-            return [
-                {
-                    id: "mathFormulaOne",
-                    name: "One Variable Formula",
-                    description: "Use Math.js to apply equation calculation. http://mathjs.org/docs/expressions/syntax.html",
-                    constants: [
-                        {
-                            name: "formula",
-                            type: "string",
-                            description: "Ex: log(sqrt(x*10))"
-                        }
-                    ],
-                    inlets: [
-                        {
-                            data_type: "NUMBER",
-                            data_unit: "",
-                            name: "x",
-                            description: "x variable"
-                        }
-                    ],
-                    outlets: {
-                        data_type: "NUMBER",
-                        data_unit: "",
-                    }
-                }, {
-                    id: "mathFormulaTwo",
-                    name: "Two Variables Formula",
-                    description: "Use Math.js to apply equation calculation. http://mathjs.org/docs/expressions/syntax.html",
-                    constants: [
-                        {
-                            name: "formula",
-                            type: "string",
-                            description: "Ex: log(sqrt(x*10)) + y"
-                        }
-                    ],
-                    inlets: [
-                        {
-                            data_type: "NUMBER",
-                            data_unit: "",
-                            name: "x",
-                            description: "x variable"
-                        }, {
-                            data_type: "NUMBER",
-                            data_unit: "",
-                            name: "y",
-                            description: "y variable"
-                        }
-                    ],
-                    outlets: {
-                        data_type: "NUMBER",
-                        data_unit: "",
-                    },
-                    // history: {
-                    //     limit: { value: 1 },
-                    //     relative_start: {value: '-1d'},
-                    // }
-                }, {
-                    id: "httpPost",
-                    name: "HTTP Post",
-                    description: "bypassing to external service",
-                    constants: [
-                        {
-                            name: "url",
-                            type: "string",
-                            description: "Endpoint url"
-                        }, {
-                            name: "add_payload",
-                            type: "string",
-                            description: "Your additional payload"
-                        }
-                    ],
-                    inlets: [
-                        {
-                            data_type: "NUMBER",
-                            data_unit: "",
-                            name: "x",
-                            description: "x variable"
-                        }
-                    ],
-                    outlets: {
-                        data_type: "string",
-                        data_unit: "",
-                    },
-                }, 
-            ];
-        } else {
-            return [
-                {
-                    id: "addNumbers",
-                    name: "Add Numbers",
-                    description: "Sum one data point and a user-defined value",
-                    constants: [
-                        {
-                            name: "adder",
-                            type: "number"
-                        }
-                    ],
-                    inlets: [
-                        {
-                            data_type: "NUMBER",
-                            data_unit: "",
-                            description: "One number"
-                        }
-                    ],
-                    outlets: {
-                        data_type: "NUMBER",
-                        data_unit: "",
-                    }
-                }    
-            ];
-        }
+        let keys = Object.keys(this._insightsInfos)
+        return keys.map((key) => {
+            return this._insightsInfos[key]
+        })
+    }
+
+    getFunctionInfo(functionId) {
+        return this._insightsInfos[functionId]
     }
 
     async addNumbers(body) {
--- src/router.js
+++ src/router.js
@@ -57,6 +57,12 @@ router.get('/info', (req, res) => {
  *   get:
  *     operationId: infoInsight
  *     description: Get info about one Insight Function
+ *     parameters:
+ *     - name: function_id
+ *       in: path
+ *       type: string
+ *       required: true
+ *       description: Which insight function to run
  *     responses:
  *       "200":
  *          description: Insight Functions successfully listed
@@ -68,7 +74,13 @@ router.get('/info', (req, res) => {
  *           $ref: '#/definitions/ErrorResponse'
  */
 router.get('/insight/:function_id', (req, res) => {
-    // not implement yet
+    let functionId = req.params.function_id
+    let funcInfo = funcs.getFunctionInfo(functionId)
+    if(funcInfo) {
+        res.json(funcInfo)
+    } else {
+        res.status(404).json({error: "function not found"})
+    }
 })
 
 /**
@@ -138,12 +150,12 @@ router.post('/process', async (req, res) => {
     let groupId = args.group_id
     let fns = funcs.getFunctions(groupId).map(obj => obj.id)
 
-    // console.log('[INFO] process-in', JSON.stringify(body))
+    console.log('[INFO] process-in', JSON.stringify(body))
     // console.log('[INFO] fns', JSON.stringify(fns))
     if(fns.includes(args.function_id)) {
         result = await funcs[args.function_id](body)
     }
-    // console.log('[INFO] process-out', JSON.stringify(result))
+    console.log('[INFO] process-out', JSON.stringify(result))
 
     res.json(result)
 })
